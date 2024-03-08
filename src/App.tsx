f5fcc326158dd19a459c37031f436cfa1e7422d9
YugBhanushali <yughbhanushali241103@gmail.com>
2024-03-08 14:25:22 +0530

    added dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import "./App.css";
 import Files from "./components/Files";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col gap-3 justify-center items-center mt-20">
+      <ModeToggle />
       <Files />
-    </>
+    </div>
   );
 }
 
